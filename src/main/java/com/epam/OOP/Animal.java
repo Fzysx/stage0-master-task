2d5276fc44c05ff5e9b0eb0aa767f5e3fb3d7352
Fzysx <44568244+Fzysx@users.noreply.github.com>
2023-08-25 19:32:15 +0600

    done

--- src/main/java/com/epam/OOP/Animal.java
+++ src/main/java/com/epam/OOP/Animal.java
@@ -1,5 +1,31 @@
 package com.epam.OOP;
 
 public class Animal {
+    private String color;
+    private int numberOfPaws;
+    private boolean hasFur;
+
+    public Animal(String color, int numberOfPaws, boolean hasFur){
+        this.color = color;
+        this.numberOfPaws = numberOfPaws;
+        this.hasFur = hasFur;
+    }
+
+    public String getDescription(){
+
+        String hasFur = "no";
+
+        if(this.hasFur){
+            hasFur = "a";
+        } else {
+            hasFur = "no";
+        }
+
+        if (numberOfPaws == 1) {
+            return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paw and " + hasFur + " fur.";
+        } else {
+            return "This animal is mostly " + this.color + ". It has " + this.numberOfPaws + " paws and " + hasFur + " fur.";
+        }
+    }
 
 }
